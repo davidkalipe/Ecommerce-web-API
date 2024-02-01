aba12db0144b794caf844bf0a2819022a2923eca
Dave_code <kalipedavid@gmail.com>
2024-02-01 11:44:35 +0000

    GetAllUser init

---
+++ Ecommerce-web-api/DTOs/UserInfoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommerce_web_api.DTOs;
+
+public class UserInfoDto
+{
+    public string Username { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+    public string? ProfileImage { get; set; }
+    public bool IsSeller { get; set; }
+    public int Graduation { get; set; }
+}
