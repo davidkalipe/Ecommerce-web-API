7704b8cebadbcddbfe6908934586344567e7da31
Dave_code <kalipedavid@gmail.com>
2024-02-01 10:50:21 +0000

    DbContext and mapping init

---
+++ Ecommerce-web-api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Ecommerce_web_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_web_api.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+    public DbSet<User> Users { get; set; }
+}
